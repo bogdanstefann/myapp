f91df1df682db09956332415b40800e99b4a1b07
bogdanstefann <bogdanstefann@gmail.com>
2023-02-07 11:46:11 +0200

    second day

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,28 @@
 class ArticlesController < ApplicationController
   def index
-    # byebug
-    # debugger
-    @test = params
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
